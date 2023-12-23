d614ce254317d713bcd7b442cbd5c64615162858
Christian-Schefe <schefechristian@gmail.com>
2023-12-23 09:55:22 +0100

    feat: day 23 - 2

--- day23/day23-2.py
+++ day23/day23-2.py
@@ -2,75 +2,88 @@ from collections import deque, defaultdict
 import sys
 
 DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-SLOPES = ['>', 'v', '<', '^']
-ANTI_SLOPES = ['<', '^', '>', 'v']
 
 sys.setrecursionlimit(100000)
+
 
 def inside(x, y, w, h):
     return x >= 0 and y >= 0 and x < w and y < h
 
 
-def is_walkable(i, x, y):
+def is_walkable(x, y):
     return inside(x, y, w, h) and grid[y][x] != '#'
 
 
-def follow(x, y, path: list, cache: dict):
-    path.append((x, y))
-    if x == end and y == h - 1:
-        print(len(path) - 1)
-        return len(path) - 1
+def follow(x, y, last_dx, last_dy):
+    steps = 1
+    while True:
+        if not is_walkable(x, y):
+            return None
+        if (x, y) == (start, 0):
+            return (x, y, steps)
+        if (x, y) == (end, h - 1):
+            return (x, y, steps)
+        n = direct_neighbours(x, y)
+        if len(n) >= 3:
+            return (x, y, steps)
+        if len(n) != 2:
+            return None
 
-    longest_path = 0
+        last_dx, last_dy = n[1][1] if n[0][1] == (-last_dx, -last_dy) else n[0][1]
+        x += last_dx
+        y += last_dy
+        steps += 1
 
-    for i, (dx, dy) in enumerate(DIRS):
-        if not is_walkable(i, x + dx, y + dy):
-            continue
 
-        if (x + dx, y + dy) in path:
-            continue
-
-        longest_path = max(longest_path, follow(x + dx, y + dy, path.copy(), cache))
-
-    return longest_path
-
+def direct_neighbours(x, y):
+    return [((x + dx, y + dy), (dx, dy)) for dx, dy in DIRS if is_walkable(x + dx, y + dy)]
 
 
 with open("input.txt") as f:
     grid = [list(x.strip()) for x in f.readlines()]
+    w, h = len(grid[0]), len(grid)
     start = grid[0].index('.')
     end = grid[-1].index('.')
-    w, h = len(grid[0]), len(grid)
+    end_i = end + (h - 1) * w
 
-    # print(follow(start, 0, [], {}))
+    nodes = [(x, y) for y, line in enumerate(grid) for x, tile in enumerate(line) if tile == '.' and len(direct_neighbours(x, y)) >= 3]
+    nodes = [(x, y) for x, y in nodes if len(direct_neighbours(x, y)) >= 3]
+    nodes.append((start, 0))
+    nodes.append((end, h - 1))
+
+    graph = {}
+    for x, y in nodes:
+        neighbours = [follow(x + dx, y + dy, dx, dy) for dx, dy in DIRS]
+        graph[y * h + x] = [(n[1] * h + n[0], n[2]) for n in neighbours if n != None]
+
+    print(graph)
 
     q = deque()
+    q.appendleft(((start, ), 0))
 
-    q.append((start, 0, [start]))
-
-    visited = set()
-    visited.add((start, 0, 1))
+    v = set()
 
     best_dist = 0
 
     while len(q) > 0:
-        x, y, path = q.pop()
-        # print(x, y)
-        if x == end and y == h - 1:
-            print(len(path))
-            best_dist = max(best_dist, len(path))
+        # print(best_dist, len(q))
 
-        for i, (dx, dy) in enumerate(DIRS):
-            nx, ny = x + dx, y + dy
-            if not is_walkable(i, nx, ny):
+        path, dist = q.pop()
+        if path[-1] == end_i:
+            if best_dist < dist:
+                print(dist)
+            best_dist = max(best_dist, dist)
+            continue
+
+        for n, d in graph[path[-1]]:
+            if n in path:
                 continue
-
-            if (nx, ny) in path:
+            new_pos = (path + (n, ), dist + d)
+            if new_pos in v:
                 continue
+            v.add(new_pos)
+            q.appendleft(new_pos)
 
-            if (nx, ny, tuple(path)) not in visited:
-                visited.add((nx, ny, tuple(path)))
-                q.append((nx, ny, path + [(nx, ny)]))
 
     # print(visited)
-    print(best_dist - 1)
+    print(best_dist)
